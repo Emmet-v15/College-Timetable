2587d52b8fbcd7990238af4d5147aa40cd72a927
Emmetv15 <emmettrey726@gmail.com>
2022-03-23 10:55:47 +0000

    new webpage

--- routes/index.js
+++ routes/index.js
@@ -1,74 +1,75 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var fs = require('fs');
-const { parse } = require('path');
+var fs = require("fs");
 
-const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 
 getLessons = () => {
-    let { data } = JSON.parse(fs.readFileSync('./data.json'))
-    let lessons = [
-        [],
-        [],
-        [],
-        [],
-        [],
-    ]
-    data.cells.forEach(( cell ) => {
+    let { data } = JSON.parse(fs.readFileSync("./data.json"));
+    let lessons = [[], [], [], [], []];
+    data.cells.forEach((cell) => {
         if (cell.length) {
-            cell = cell.slice(cell.length-5);
+            cell = cell.slice(cell.length - 5);
             for (let i = 0; i < cell.length; i++) {
                 const slot = cell[i];
                 if (slot.length === 0) continue;
-                lessons[i].push(slot.match(/(?<startTime>\d\d:\d\d) - (?<endTime>\d\d:\d\d) (?<course>.*) A Level .* (?<room>.*)/).groups);
-            };
+                lessons[i].push(
+                    slot.match(/(?<startTime>\d\d:\d\d) - (?<endTime>\d\d:\d\d) (?<course>.*) A Level .* (?<room>.*)/)
+                        .groups
+                );
+            }
         }
     });
-    return lessons
-}
+    return lessons;
+};
 
 getDay = () => {
     let lessons = getLessons();
     function addDay() {
         schoolDay++;
-        schoolDay = schoolDay >= 5? schoolDay-5 : schoolDay;
+        schoolDay = schoolDay >= 5 ? schoolDay - 5 : schoolDay;
     }
-    
+
     function lessonsOver() {
-        return lessons[schoolDay] && !lessons[schoolDay].map(lesson => {
-            let date = new Date();
-            date.setHours(lesson.endTime.split(":")[0]);
-            return date < new Date();
-        }).includes(false)
+        return (
+            lessons[schoolDay] &&
+            !lessons[schoolDay]
+                .map((lesson) => {
+                    let date = new Date();
+                    date.setHours(lesson.endTime.split(":")[0]);
+                    return date < new Date();
+                })
+                .includes(false)
+        );
     }
-    
+
     function checkLessons() {
         while (!lessons[schoolDay] || lessons[schoolDay].length == 0) {
             addDay();
         }
     }
     let schoolDay = new Date().getUTCDay();
-    schoolDay = schoolDay >= 7? schoolDay-7 : schoolDay;
-    
+    schoolDay = schoolDay >= 7 ? schoolDay - 7 : schoolDay;
+
     if (schoolDay >= 6 || schoolDay == 0) {
-        schoolDay = 0
-    } else schoolDay--
-    
+        schoolDay = 0;
+    } else schoolDay--;
+
     if (!lessons[schoolDay] || lessons[schoolDay].length == 0) {
-        checkLessons()
+        checkLessons();
     } else if (lessonsOver()) {
         addDay();
-        checkLessons()
+        checkLessons();
     }
 
-    return schoolDay
-}
+    return schoolDay;
+};
 
-router.get('/', (req, res, next) => {
-    res.render('index', {
+router.get("/", (req, res, next) => {
+    res.render("index", {
         lessons: getLessons()[getDay()],
         day: days[getDay()],
-        refreshTimestamp: JSON.parse(fs.readFileSync('./data.json')).data.timestamp
+        refreshTimestamp: JSON.parse(fs.readFileSync("./data.json")).data.timestamp,
     });
 });
 
